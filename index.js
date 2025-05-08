02a913f96b5a7d3a45465115264d500fbe5917df
S Pavan <sepuripavan07@gmail.com>
2025-05-08 14:35:10 +0530

    1st, direct running through directory

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express=require("express");
-const path=requie("path");
-
 const app=express();
 
 const port =3000;
 
 app.set("view engine","ejs");
-app.set("views",path)
+
 app.get("/",(req,res)=>{
     res.render("home.ejs");
 })
