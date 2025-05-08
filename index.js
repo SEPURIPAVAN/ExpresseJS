89de28f5e527de3d7fc4a6708432d28cc5d1138a
S Pavan <sepuripavan07@gmail.com>
2025-05-08 14:53:01 +0530

    run through parent dir

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express=require("express");
 const app=express();
 
+            const path=require("path"); //so your writing this 2 lines of code, 1
+
 const port =3000;
 
 app.set("view engine","ejs");
 
+            app.set("views",path.join(__dirname,"/views")) //2
+
 app.get("/",(req,res)=>{
-    res.render("home.ejs");
+    res.render("home.ejs");  //this dosent work if u run code through the parent dir
 })
+
+            app.get("/home",(req,res)=>{
+                res.send("hello");
+            })
  
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
